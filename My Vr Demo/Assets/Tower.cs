cad6ce27cab6f8dbf506c4f0ca2b377762d13030
Alexander Johr <alexander.johr@gmail.com>
2020-05-20 18:06:31 +0200

    Feature tower (#1)
    * Imported Model of Tower
    
    * Added Tower Script
    
    * Added Tower to Level 1
    
    * Added Second Tower
    
    * Added Rotation
    
    * Corrected Rotation

---
+++ My Vr Demo/Assets/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(this.transform.localRotation.eulerAngles.x
+                                                  , this.transform.localRotation.eulerAngles.y + 1
+                                                  , this.transform.localRotation.eulerAngles.z);
+    }
+}
