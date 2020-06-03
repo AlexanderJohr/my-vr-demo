b8df65e6b6bba3d059fa2b13d75f14065dca98fd
Alexander Johr <alexander.johr@gmail.com>
2020-06-03 17:30:45 +0200

    Added Occlusion culling

---
+++ My Vr Demo/Assets/Book.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool mausTasteGedrückt = Input.GetMouseButtonDown(0);
+
+        if (mausTasteGedrückt) {
+
+            Vector3 vectorVonKameraZuBuch = transform.position - mainCamera.transform.position;
+
+            float winkel = Vector3.Dot(vectorVonKameraZuBuch.normalized, mainCamera.transform.forward.normalized);
+            if (winkel > 0.98) {
+                gameObject.GetComponent<Rigidbody>()
+                    .AddForce(
+                    new Vector3(Random.Range(-100f, 100f)
+                    , Random.Range(-100f, 100f)
+                    , Random.Range(-1000f, 0f)));
+            }
+
+        }
+    }
+}
