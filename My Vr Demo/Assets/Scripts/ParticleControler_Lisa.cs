07a50df0717d7bee2b35822439b1e29dfa711bec
Lisa Renz <u32847@hs-harz.de>
2020-06-10 18:07:53 +0200

    Create Particle System
    - added particle system in scene
    - edited color, sphere, number
    - added script to influence particles
    - move all scripts in right folder

---
+++ My Vr Demo/Assets/Scripts/ParticleControler_Lisa.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControler_Lisa : MonoBehaviour
+{
+    ParticleSystem ps; //Attribut oder Feld
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //componenten holen, die wir im weiteren Verlauf benötigen
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool mousebuttonIsClicked = Input.GetMouseButtonDown(0);
+
+        if (mousebuttonIsClicked)
+        {
+            ps.Play();
+        }
+        
+    }
+}
