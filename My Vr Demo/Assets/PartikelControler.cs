5e0ec6f432beecd9911706bfc8dfee373d98ddae
Alexander Johr <alexander.johr@gmail.com>
2020-06-10 18:39:33 +0200

    Added VR TK and one example of a raycast which starts a particle system

---
+++ My Vr Demo/Assets/PartikelControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartikelControler : MonoBehaviour
+{
+    ParticleSystem ps; // Feld oder Attribut
+
+    void Start()
+    {
+        
+
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+
+
+        
+
+
+
+        bool maustasteWurdeGedrückt = Input.GetMouseButtonDown(0);
+
+        if (maustasteWurdeGedrückt) {
+
+            // Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 1 << 8;
+
+            RaycastHit hit;
+
+            bool wurdeGetroffen = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, layerMask);
+
+            if (wurdeGetroffen)
+            {
+                ps.Play();
+            }
+            else
+            {
+                
+            }
+
+
+
+
+
+            
+        }
+
+    }
+}
