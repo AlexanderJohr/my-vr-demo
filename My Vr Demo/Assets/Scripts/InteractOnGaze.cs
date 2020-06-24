59da4286fb30e831a4ae1c2bc6ba608b21b52631
Alexander Johr <alexander.johr@gmail.com>
2020-06-24 18:25:17 +0200

    Modified InteractOnGazeScript to play Effect on Gaze

--- My Vr Demo/Assets/Scripts/InteractOnGaze.cs
+++ My Vr Demo/Assets/Scripts/InteractOnGaze.cs
@@ -1,18 +1,66 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractOnGaze : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool schaueBereitsDrauf;
+
+    [SerializeField]
+    private float winkelToleranz;
+
+    [SerializeField]
+    private ParticleSystem particleSystemGameObject; 
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        var a = Camera.main.transform.position;
+        var b = transform.position;
+
+        var ab = b - a;
+        var abn = ab.normalized;
+
+        var ctf = Camera.main.transform.forward;
+
+        // -1 = Entgegengesetzt
+        //  0 = senkrecht aufeinander
+        //  1 = gleiche Richtung
+        var winkel = Vector3.Dot(ctf, abn); // -1 ... 0 ... 0.9 ... 1 
+
+
+        // z.B.  wT = 0.1    1-wT=0.9    w> 0.9                 ⬇   ↔   ⬇
+        //                                         -1 ... 0 ... 0.9 ... 1     
+        bool schaueGeradeDrauf = winkel > (1 - winkelToleranz);
+
+         
+
+        if (!schaueBereitsDrauf && schaueGeradeDrauf)
+        {
+            schaueBereitsDrauf = true;
+
+            AktiviereEffekt();
+        }
+        else if (schaueBereitsDrauf && !schaueGeradeDrauf) {
+            schaueBereitsDrauf = false;
+
+            DeaktiviereEffekt();
+        }
+ 
+    }
+
+    private void DeaktiviereEffekt()
+    {
+        particleSystemGameObject.Stop();
+    }
+
+    private void AktiviereEffekt()
+    {
+        particleSystemGameObject.Play();
     }
 }
