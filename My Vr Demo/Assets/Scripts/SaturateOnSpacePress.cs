23bf763a867f5bdc81ffe5705f75ac31a68749ce
Alexander Johr <alexander.johr@gmail.com>
2020-07-08 21:30:21 +0200

    Made Saturation Switch on Press of Space

---
+++ My Vr Demo/Assets/Scripts/SaturateOnSpacePress.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SaturateOnSpacePress : MonoBehaviour
+{
+    PostProcessVolume postProcessVolume;
+    ColorGrading colorGrading;
+
+    private bool saturation;
+
+    public bool Saturation
+    {
+        get { return saturation; }
+        set {
+            saturation = value;
+
+            if (saturation)
+            {
+                colorGrading.saturation.value = 0;
+            }
+            else {
+                colorGrading.saturation.value = -100;
+            }
+        }
+    }
+
+
+
+    void Start()
+    {
+        postProcessVolume = FindObjectOfType<PostProcessVolume>();
+
+        
+        postProcessVolume.profile.TryGetSettings<ColorGrading>(out colorGrading);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        bool spaceIsPressed = Input.GetKeyDown(KeyCode.Space);
+
+
+        if (spaceIsPressed) {
+            Saturation = !Saturation;
+        }
+    }
+}
